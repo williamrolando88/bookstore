b2edcdf963fc1d13d8935d6d41643b70d6308474
williamrolando88 <william.rolando88@hotmail.com>
2022-01-17 17:53:13 -0500

    Add MVP styling to AddNew component

--- src/components/AddNew.jsx
+++ src/components/AddNew.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 
 const AddNew = () => {
   return (
-    <div>
-      <h2>Add new book</h2>
-      <form className="flex gap-6" action="submit">
-        <input className="grow border" type="text" placeholder="Book title" />
-        <select className="w-1/4 border" name="category" id="category">
+    <div className="border-t pt-7">
+      <h2 className="text-xl uppercase">Add new book</h2>
+      <form className="flex gap-6 mt-5" action="submit">
+        <input
+          className="grow border px-4 py-2"
+          type="text"
+          placeholder="Book title"
+        />
+        <select
+          className="w-1/4 border px-4 py-2"
+          name="category"
+          id="category">
           <option value="" disabled selected>
             Category
           </option>
@@ -16,7 +23,9 @@ const AddNew = () => {
           <option value="Option4">Option4</option>
           <option value="Option5">Option5</option>
         </select>
-        <button className="w-1/4 " type="submit">
+        <button
+          className="w-44 px-6 text-white text-xs bg-sky-600 rounded"
+          type="submit">
           Add Book
         </button>
       </form>
