024054d41c3e0eca4480467fcc70593b8432da5b
williamrolando88 <william.rolando88@hotmail.com>
2022-01-17 18:03:25 -0500

    Add props as data to render

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
-const Book = () => {
+const Book = (props) => {
+  const {
+    book: { title, author, category, chapter, progress },
+  } = props;
   return (
     // Unitary container
     <div className="border flex justify-between items-center p-6 gap-4 divide-x-2">
@@ -9,11 +12,9 @@ const Book = () => {
         <div>
           {/* Book description */}
           <div>
-            <h3 className="text-xs font-semibold text-stone-700">
-              Book category
-            </h3>
-            <h2 className="text-xl">Book title</h2>
-            <h4 className="text-xs text-sky-600">Author</h4>
+            <h3 className="text-xs font-semibold text-stone-700">{category}</h3>
+            <h2 className="text-xl">{title}</h2>
+            <h4 className="text-xs text-sky-600">{author}</h4>
           </div>
           {/* Actions buttons */}
           <div className="flex divide-x gap-2 text-sky-600 mt-5">
@@ -33,7 +34,7 @@ const Book = () => {
         <div className="flex w-1/3 items-center gap-5">
           <div className="rounded-full w-[4.5rem] h-[4.5rem] bg-sky-600"></div>
           <div className="flex flex-col items-center">
-            <p className="text-3xl font-bold">10%</p>
+            <p className="text-3xl font-bold">{`${progress}%`}</p>
             <p className="text-sm">Completed</p>
           </div>
         </div>
@@ -42,7 +43,7 @@ const Book = () => {
       {/* Chapter and update progress container */}
       <div className="flex flex-col w-1/4 pl-4">
         <p className="text-xs">Current chapter</p>
-        <p className="">Chapter 1</p>
+        <p className="">{chapter}</p>
         <button
           className="bg-sky-600 self-start px-6 uppercase py-2 text-white rounded mt-5 text-sm font-thin"
           type="button">
