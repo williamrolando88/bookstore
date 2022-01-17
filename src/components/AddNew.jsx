b0af92c61965bfefe703b122c7b83e0f602d914a
williamrolando88 <william.rolando88@hotmail.com>
2022-01-17 17:20:14 -0500

    Create add new book form elements

--- src/components/AddNew.jsx
+++ src/components/AddNew.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
 
 const AddNew = () => {
-  return <div>Click here to add new book</div>;
+  return (
+    <div>
+      <h2>Add new book</h2>
+      <form className="flex gap-6" action="submit">
+        <input className="grow border" type="text" placeholder="Book title" />
+        <select className="w-1/4 border" name="category" id="category">
+          <option value="" disabled selected>
+            Category
+          </option>
+          <option value="Option1">Option1</option>
+          <option value="Option2">Option2</option>
+          <option value="Option3">Option3</option>
+          <option value="Option4">Option4</option>
+          <option value="Option5">Option5</option>
+        </select>
+        <button className="w-1/4 " type="submit">
+          Add Book
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default AddNew;
