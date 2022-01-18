cc07eea51a00efae4027d7cada77dab765eb347d
williamrolando88 <william.rolando88@hotmail.com>
2022-01-18 12:29:44 -0500

    Add state management functions to AddNew component

--- src/components/AddNew.jsx
+++ src/components/AddNew.jsx
@@ -1,28 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const AddNew = () => {
+const AddNew = (props) => {
+  const { setBook } = props;
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
   return (
     <div className="border-t pt-7">
       <h2 className="text-xl uppercase">Add new book</h2>
-      <form className="flex gap-6 mt-5" action="submit">
+      <form
+        className="flex gap-6 mt-5"
+        action="submit"
+        onSubmit={() => setBook(title, author)}>
         <input
           className="grow border px-4 py-2"
           type="text"
           placeholder="Book title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
         />
-        <select
+        <input
           className="w-1/4 border px-4 py-2"
-          name="category"
-          id="category">
-          <option value="" disabled selected>
-            Category
-          </option>
-          <option value="Option1">Option1</option>
-          <option value="Option2">Option2</option>
-          <option value="Option3">Option3</option>
-          <option value="Option4">Option4</option>
-          <option value="Option5">Option5</option>
-        </select>
+          name="author"
+          placeholder="Book author"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
         <button
           className="w-44 px-6 text-white text-xs bg-sky-600 rounded"
           type="submit">
