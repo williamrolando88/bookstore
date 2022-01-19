897b21aed04de7f0b09cfde70b240e5c2b520336
williamrolando88 <william.rolando88@hotmail.com>
2022-01-19 09:39:39 -0500

    Implementation of useSelector hook, fix linters errors

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { v4 } from 'uuid';
-import { addBook } from '../redux/books/books.js';
-import store from '../redux/configureStore.js';
-import AddNew from './AddNew.jsx';
-import Book from './Book.jsx';
+import { addBook } from '../redux/books/books';
+import store from '../redux/configureStore';
+import AddNew from './AddNew';
+import Book from './Book';
 
 const Books = () => {
-  const [books, setBooks] = useState([]);
+  let books;
 
   const dispatch = useDispatch();
 
@@ -20,29 +20,25 @@ const Books = () => {
       chapter: `Chapter ${Math.floor(Math.random() * 20)}`,
       progress: Math.floor(Math.random() * 100),
     };
-
     dispatch(addBook(book));
   };
 
   useEffect(() => {
     const unsubscribe = store.subscribe(() => {
-      setBooks(store.getState().booksReducer);
+      books = store.getState().booksReducer;
     });
     return () => {
-      unsubscribe;
+      unsubscribe();
     };
   }, []);
 
-  useEffect(() => {
-    setBooks(store.getState().booksReducer);
-    return () => {};
-  }, [store.getState().booksReducer]);
+  books = useSelector((state) => state.booksReducer);
 
   return (
     <div className="px-24 py-10">
       <div className="flex flex-col gap-6 mb-10">
-        {books.map((book, index) => (
-          <Book key={index} book={book} />
+        {books.map((book) => (
+          <Book key={book.id} book={book} />
         ))}
       </div>
       <AddNew addBook={(title, author) => handleAddBook(title, author)} />
